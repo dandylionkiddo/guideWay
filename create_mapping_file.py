28b09edce9c235c8dc381d8df93bde044d0ffce5
Whililik <mensa1229@gmail.com>
2025-08-18 15:42:40 +0900

    class mapping updated

--- create_mapping_file.py
+++ create_mapping_file.py
@@ -2,19 +2,18 @@ import json
 
 # Define file paths with absolute paths
 input_txt_path = 'D:/AIffel/EfficientViT/efficientvit/new_classes.txt'
-output_json_path = 'D:/AIffel/EfficientViT/data/mapillary-vistas-dataset_public_v2.0/custom_class_mapping.json'
+output_json_path = 'D:/AIffel/EfficientViT/efficientvit/applications/efficientvit_seg/custom_class_mapping.json'
 
 # Define the new class order
 new_class_names = [
-    'sidewalk', 'curb-cut', 'crosswalk', 'road', 'minor-road', 'bike-lane', 'curb', 'terrain',
-    'car', 'truck', 'bus', 'motorcycle', 'bicycle', 'person', 'rider', 'vegetation', 'sky', 'water',
-    'sign', 'traffic-cone', 'puddle', 'pothole', 'manhole', 'catch-basin', 'bench', 'pole',
-    'building', 'wall', 'fence', 'void'
+    'sidewalk', 'curb', 'curb-cut', 'crosswalk', 'road', 'minor-road', 'bike-lane', 'terrain',
+    'cautious-zone', 'repair-zone', 'car-four-wheeled', 'motorcycle', 'micromobility', 'other-vehicle', 'person',
+    'pothole', 'manhole', 'drain', 'background'
 ]
 
-# Create a mapping from new class name to new ID (0-29 for main classes, 255 for void)
-new_name_to_id = {name: i for i, name in enumerate(new_class_names) if name != 'void'}
-new_name_to_id['void'] = 255
+# Create a mapping from new class name to new ID (0-17 for main classes, 255 for background)
+new_name_to_id = {name: i for i, name in enumerate(new_class_names) if name != 'background'}
+new_name_to_id['background'] = 255
 
 class_mapping = {}
 
@@ -37,8 +36,8 @@ with open(input_txt_path, 'r', encoding='utf-8') as f:
             continue
             
         # Handle complex string like '115116' -> '115', '116'
-        if '115116' in old_ids_str:
-            old_ids_str = old_ids_str.replace('115116', '115, 116, ')
+        # if '115116' in old_ids_str:
+        #     old_ids_str = old_ids_str.replace('115116', '115, 116, ')
 
         old_ids = [int(x.strip()) for x in old_ids_str.split(',') if x.strip()]
         
